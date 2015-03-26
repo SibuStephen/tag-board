ba6d0a2e9d556121ffdbb297f7d97dc0dd9ba6ae
sibstephen <sibustephen591@gmail.com>
2015-03-26 21:00:37 +0530

    Changes added on 26ht at 8:58pm

--- controller/Angular-controller.js
+++ controller/Angular-controller.js
@@ -3,7 +3,14 @@
 var tagboardController = angular.module('tag-board', []);
 
 tagboardController.controller('tagboardCtrl', function ($http, $scope) {
-  $http.get('tabledata'+'.json').success(function(data) {
+  $http.get('controller/tabledata.json').success(function(data) {
       $scope.tag_list = data;
     });
 });
+
+// tagboardController.controller('list-of-tags',function ($http , $scope){
+//  $http.get('table-description'+'.json').sucess(function(data){
+//     $scope.tag-lists -data;
+//  });
+
+// });
