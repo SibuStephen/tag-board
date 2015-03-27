245c8118ef19603e6c0186e98617c958663c78dd
sibstephen <sibustephen591@gmail.com>
2015-03-27 14:37:40 +0530

    angular search

--- controller/Angular-controller.js
+++ controller/Angular-controller.js
@@ -2,17 +2,19 @@
 
 var tagboardController = angular.module('tag-board', []);
 
-tagboardController.controller('tagboardCtrl', function ($http, $scope) {
+tagboardController.controller('tagboardCtrl', ['$http','$scope',
+  function ($http, $scope) {
   $http.get('controller/tabledata.json').success(function(data) {
       $scope.tag_list = data;
     });
-});
+}]);
 
-tagboardController.controller('list-of-tags',function ($http , $scope){
- $http.get('table-description'+'.json').sucess(function(data){
-    $scope.tag-lists-data;
- });
-});
+// tagboardController.controller('listagCtrl', ['$http','$scope',
+//   function ($http, $scope) {
+//  $http.get('controller/table-description.json').success(function(data){
+//     $scope.tag_lists_data=data;
+//    });
+// }]);
 
 // tagboardController.controller('list-search',function ($http , $scope){
 //  $http.get('table-description'+'.json').sucess(function(data){
