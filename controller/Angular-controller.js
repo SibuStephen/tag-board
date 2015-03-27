8a70de47bd31d7f7af9cb383007ef4fe488be042
sibstephen <sibustephen591@gmail.com>
2015-03-27 12:40:35 +0530

    Small changes in setting section

--- controller/Angular-controller.js
+++ controller/Angular-controller.js
@@ -8,9 +8,14 @@ tagboardController.controller('tagboardCtrl', function ($http, $scope) {
     });
 });
 
-// tagboardController.controller('list-of-tags',function ($http , $scope){
+tagboardController.controller('list-of-tags',function ($http , $scope){
+ $http.get('table-description'+'.json').sucess(function(data){
+    $scope.tag-lists -data;
+ });
+});
+
+// tagboardController.controller('list-search',function ($http , $scope){
 //  $http.get('table-description'+'.json').sucess(function(data){
 //     $scope.tag-lists -data;
 //  });
 
-// });
